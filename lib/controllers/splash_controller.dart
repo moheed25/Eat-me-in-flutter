930e48bfe124dff52c6613c84354123c6c4da626
unknown <moheed25@gmail.com>
2022-11-02 09:38:31 +0500

    work complete

---
+++ lib/controllers/splash_controller.dart
@@ -0,0 +1,31 @@
+import 'package:eat/navigator.dart';
+import 'package:eat/pages/introduction.dart';
+
+import 'package:get/get.dart';
+//
+import 'package:get_storage/get_storage.dart';
+
+class SplashController extends GetxController {
+  /// For showing introduction screen just once
+  final box = GetStorage();
+  bool get showIntro => box.read('showIntro') ?? true;
+  void changeBool(bool val) {
+    box.write('showIntro', val);
+    update();
+  }
+
+  @override
+  void onInit() {
+    Future.delayed(
+      const Duration(seconds: 10),
+      () {
+        Get.offAll(
+            () => showIntro == true
+                ? const IntroductionPage()
+                : const MainNavigator(),
+            transition: Transition.cupertino);
+      },
+    );
+    super.onInit();
+  }
+}
